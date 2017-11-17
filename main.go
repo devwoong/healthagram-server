5bfdb9faded934704009f7a9357ec4c99000b958
chulwoong park <chulwoongpark@chulwoongui-MacBook-Pro.local>
2017-11-17 17:11:04 +0900

    ip, port log

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
+	"text/template"
 
 	"github.com/gorilla/mux"
 )
@@ -10,14 +12,30 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/test", testHTTP).Methods("GET")
-	server := &http.Server{
-		Handler: r,
-		Addr:    "localhost:8000",
-	}
-	server.ListenAndServe()
+	r.HandleFunc("/Html", htmlTestHTTP).Methods("GET")
+	//	server := &http.Server{
+	//		Handler: r,
+	//		Addr:    "localhost:8000",
+	//	}
+	//	server.ListenAndServe()
+	http.ListenAndServe(":8000", r)
 }
 
 func testHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello"))
-	log.Print("request from other user")
+
+	ip, port, err := net.SplitHostPort(r.RemoteAddr)
+
+	if err != nil {
+		log.Print("err ")
+	}
+	log.Print("request from other user IP : " + ip + " port : " + port)
+}
+
+func htmlTestHTTP(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("test.html")
+	if err != nil {
+		panic(err)
+	}
+	tmpl.Execute(w, nil)
 }
